62212d36f6959f5893509c5891950592deb21263
Krishnanshu <krishnanshu.khanna@gmail.com>
2024-07-08 18:57:49 +0530

    feat: Add key property to components in map function

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "./ui/dialog";
 import UserProfileForm, {
 	UserFormData,
 } from "@/forms/user-profile-form/UserProfileForm";
@@ -53,6 +53,7 @@ const CheckoutButton = ({ onCheckout, disabled, isLoading }: Props) => {
 				</Button>
 			</DialogTrigger>
 			<DialogContent className='max-w-[425px] md:min-w-[700px] bg-gray-50'>
+				<DialogTitle></DialogTitle>
 				<UserProfileForm
 					currentUser={currentUser}
 					onSave={onCheckout}
